872bfef45031ef33f785b369d289130875c54602
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2016-12-20 16:02:37 +0100

    change regexp

--- functions/normalize_name.pp
+++ functions/normalize_name.pp
@@ -1,5 +1,5 @@
 function g_firewall::normalize_name(String $name, String $protocol){
-  $matches = $name.match(/^([0-9]+)(.IPv[0-9])?\s+(.*?)$/)
+  $matches = $name.match(/^(.*?)(.IPv[0-9])?\s+(.*?)$/)
   
   "${matches[1]}.${protocol} ${matches[3]}"
 }
