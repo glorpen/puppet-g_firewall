644eb18f6a83f5f59a3c4efee28874229c91020e
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2018-02-25 08:59:01 +0100

    removed auto creating firewallchain's

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,11 +1,4 @@
 class g_firewall::params {
-
-  $_mangle_chain = {'mangle' => ['PREROUTING', 'INPUT', 'FORWARD', 'OUTPUT', 'POSTROUTING']}
-  $_filter_chain = {'filter' => ['INPUT', 'FORWARD', 'OUTPUT']}
-  $_raw_chain = {'raw' => ['PREROUTING', 'OUTPUT']}
-
-  $_nat_chain = {'nat' => ['PREROUTING', 'INPUT', 'OUTPUT', 'POSTROUTING']}
-
   include ::stdlib
 
   $protocols = [
@@ -18,9 +11,4 @@ class g_firewall::params {
       default => 'IPv6'
     },
   ].filter |$v| { $v != undef }
-
-  $chains = {
-    'IPv4' => merge($_mangle_chain, $_filter_chain, $_raw_chain, $_nat_chain),
-    'IPv6' => merge($_mangle_chain, $_filter_chain, $_raw_chain, $_nat_chain)
-  }
 }
--- manifests/setup.pp
+++ manifests/setup.pp
@@ -5,19 +5,8 @@ class g_firewall::setup(
 
   if $initialize {
 
-    $::g_firewall::params::chains.each | $protocol, $tables | {
-      $tables.each | $table, $chains | {
-        $chains.each | $chain | {
-          firewallchain { "${chain}:${table}:${protocol}":
-            ensure => present,
-            purge  => true,
-          }
-        }
-      }
-    }
-
     resources { 'firewall':
-      purge => false,
+      purge => true,
     }
 
     resources { 'firewallchain':
@@ -28,6 +17,9 @@ class g_firewall::setup(
       before  => Class['g_firewall::rules::post'],
       require => Class['g_firewall::rules::pre'],
     }
+    Firewallchain {
+      purge => true
+    }
 
     class { ['g_firewall::rules::pre', 'g_firewall::rules::post']: }
 
@@ -37,7 +29,5 @@ class g_firewall::setup(
     g_firewall_syschain { 'default' :
       regex => '^(PREROUTING|POSTROUTING|BROUTING|INPUT|FORWARD|OUTPUT)$'
     }
-
   }
-
 }
