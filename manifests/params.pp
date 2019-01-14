cde9a24d16adb41a40504abc96d7173ff4ea36a6
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2019-01-14 16:29:38 +0100

    ipv6 should be now supported everywhere

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,14 +1,4 @@
 class g_firewall::params {
   include ::stdlib
-
-  $protocols = [
-    $facts['iptables_version']?{
-      undef => undef,
-      default => 'IPv4'
-    },
-    $facts['ip6tables_version']?{
-      undef => undef,
-      default => 'IPv6'
-    },
-  ].filter |$v| { $v != undef }
+  $protocols = ['IPv4', 'IPv6']
 }
